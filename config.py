89abd5a089f4bafb99488897f08039be593eadf1
Kenneth Leung <kennethleung.ty@gmail.com>
2022-02-09 15:34:42 +0800

    Amend SSL of configuration parameters

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ import pymysql
 
 USERNAME = 'admin'  # Replace with your master username 
 PASSWORD = 'XXXXX'  # Replace with your RDS instance password
-ENDPOINT = "XXXXXXX.XXXXXXX.ap-southeast-1.rds.amazonaws.com"  # Replace with your RDS endpoint
+ENDPOINT = "XXXXX.XXXXX.ap-southeast-1.rds.amazonaws.com"  # Replace with your RDS endpoint
 PORT = 3306   # Replace with the RDS instance port
-REGION = "ap-southeast-1b"  # Replace with your AWS region
+REGION = "ap-southeast-1"  # Replace with your AWS region
 DBNAME = 'schema1'  # Replace with the name of your SCHEMA in MySQL workbench
-SSL_CA = "ssl/ap-southeast-1-bundle.pem"  # Replace with the folder location of your SSL bundle
+SSL_CA = "ssl/rds-combined-ca-bundle.pem"  # Replace with the folder location of your SSL bundle
 CURSORCLASS = pymysql.cursors.DictCursor  # No need to modify this
