e138cb74320bb2771b7c5cee674401d3e92e74fe
Kenneth Leung <kennethleung.ty@gmail.com>
2022-02-09 14:28:41 +0800

    Update config and example notebook

--- config.py
+++ config.py
@@ -1,12 +1,10 @@
 import pymysql
 
-ACCESS_KEY_USER_KENNETH = 'xxxxx'
-SECRET_KEY_USER_KENNETH = 'xxxxx'
-USERNAME = 'admin' # Master username
-PASSWORD = 'xxx' # Password of RDS Credential Settings
-ENDPOINT = "client-database.xxxxx.ap-southeast-1.rds.amazonaws.com"
-PORT = 3306
-REGION = "ap-southeast-1"
-DBNAME = "client-database"
-CURSORCLASS = pymysql.cursors.DictCursor
-SSL_CA = "ssl/ap-southeast-1-bundle.pem"
+USERNAME = 'admin'  # Replace with your master username 
+PASSWORD = 'XXXXX'  # Replace with your RDS instance password
+ENDPOINT = "XXXXXXX.XXXXXXX.ap-southeast-1.rds.amazonaws.com"  # Replace with your RDS endpoint
+PORT = 3306   # Replace with the RDS instance port
+REGION = "ap-southeast-1b"  # Replace with your AWS region
+DBNAME = 'schema1'  # Replace with the name of your SCHEMA in MySQL workbench
+SSL_CA = "ssl/ap-southeast-1-bundle.pem"  # Replace with the folder location of your SSL bundle
+CURSORCLASS = pymysql.cursors.DictCursor  # No need to modify this
